ce4219019afcbea99a5da8d8582a4e337ad86307
Samuel Hartmann <hartmann.samuel@gmx.de>
2022-06-20 08:35:09 +0200

    solution for dragover (10:50)
    - JavaScript for dragover handling
    - added screen capture (mp4) of state of the app

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ draggables.forEach(draggable => {
         draggable.classList.remove('dragging')
     })
 })
+
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+        console.log('drag over')
+        e.preventDefault()
+        const draggable = document.querySelector('.dragging')
+        container.appendChild(draggable)
+    })
+})
